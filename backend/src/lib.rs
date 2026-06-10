28813af0bb4615ab8161a82a4bbf5bb5ef99ff26
klaidoshka <klaidas.usas@gmail.com>
2026-06-10 20:35:39 +0300

    feat: Add additional plugins for dialog/file-opening purposes

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -22,6 +22,7 @@ pub fn run() {
       Ok(())
     })
     .plugin(tauri_plugin_opener::init())
+    .plugin(tauri_plugin_dialog::init())
     .invoke_handler(tauri::generate_handler![
       cmd::project::create_project,
       cmd::project::edit_project,
